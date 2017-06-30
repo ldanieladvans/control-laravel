b7c48a446aa813a939cb968a7c2ab44fd3f07b19
ADVANS\ldaniel <ldaniel@advans.mx>
2017-06-30 16:26:09 -0500

    Subiendo config ssh 2

--- control/config/database.php
+++ control/config/database.php
@@ -44,7 +44,7 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+        'mysql-orig' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
@@ -59,6 +59,20 @@ return [
             'engine' => null,
         ],
 
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1:13306',
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
         //AddDB
         
     ],
