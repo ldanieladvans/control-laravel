d25c399cefd67e0e9e4775bbb70ee4625c63b38c
ADVANS\ldaniel <ldaniel@advans.mx>
2017-06-30 16:49:14 -0500

    Subiendo config ssh 6. Ok

--- control/config/database.php
+++ control/config/database.php
@@ -1,10 +1,5 @@
 <?php
 
-if(env('SSH_TUN',false)){
-    shell_exec("ssh -i ../dev-capas.pem -f -L 13306:127.0.0.1:3306 centos@172.31.2.220 >> sshlogfile");
-}
-
-
 return [
 
     /*
@@ -42,21 +37,6 @@ return [
             'driver' => 'sqlite',
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
-        ],
-
-        'mysql-orig' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
         ],
 
         'mysql' => [
