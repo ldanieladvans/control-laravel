8937a2eb5664e997f64e0fa8482f7dcb57ec919f
ADVANS\ldaniel <ldaniel@advans.mx>
2017-07-03 13:50:46 -0500

    Subiendo bloqueo de usuario tras intentpo de autenticacion y traducciones

--- control/app/Http/Controllers/Auth/LoginController.php
+++ control/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -40,5 +41,12 @@ class LoginController extends Controller
     public function username()
     {
         return 'usrc_nick';
+    }
+
+    protected function hasTooManyLoginAttempts(Request $request)
+    {
+        return $this->limiter()->tooManyAttempts(
+            $this->throttleKey($request), 3, 1
+        );
     }
 }
