cc53abb6ed8c597765fc39d521b3effdb458ff32
ADVANS\ldaniel <ldaniel@advans.mx>
2017-08-17 12:11:34 -0500

    Subiendo actualizacion de envio de correo 1

--- control/resources/views/mails/clientcreate.blade.php
+++ control/resources/views/mails/clientcreate.blade.php
@@ -1,7 +1,13 @@
 Se ha creado una cuenta. Los datos de acceso son:
 <div>
+    Cuenta: {{ $rfc }}
+</div>
+<div>
     Usuario: {{ $user }}
 </div>
 <div>
     Contraseña: {{ $password }}
+</div>
+<div>
+    Liga: {{ $link }}
 </div>
